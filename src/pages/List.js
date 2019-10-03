3279417bf8eb3a1c945ae1b1d019e17dc5f7fdea
Caio Carvalho <ccarvalho451@gmail.com>
2019-10-03 17:21:28 -0300

    Tela de login e listagem de spots

---
+++ src/pages/List.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+
+import { SafeAreaView, StyleSheet, ScrollView, Image, AsyncStorage } from 'react-native';
+
+import list from '../components/SpotList'
+import logo from '../assets/logo.png';
+import SpotList from '../components/SpotList';
+
+export default function List() {
+    const [techs, SetTechs] = useState([])
+
+    useEffect(() => {
+        AsyncStorage.getItem('techs').then(storagedTechs => {
+            const techsArray = storagedTechs.split(',').map(tech => tech.trim());
+
+            SetTechs(techsArray);
+        })
+    }, [])
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <Image style={styles.logo} source={logo} />
+            <ScrollView>
+                {techs.map(tech => <SpotList key={tech} tech={tech} />)}
+            </ScrollView>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+
+    logo: {
+        height: 32,
+        resizeMode: 'contain',
+        alignSelf: 'center',
+        marginTop: 40
+    }
+})
