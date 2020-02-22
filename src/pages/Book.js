fce137a1dfd26cb626df75864e2f439b3cb09db5
Caio Carvalho <ccarvalho451@gmail.com>
2020-02-22 01:09:52 -0300

    fix indentation

--- src/pages/Book.js
+++ src/pages/Book.js
@@ -1,93 +1,110 @@
-import React, { useState } from 'react';
-import { Text, TextInput, View, SafeAreaView, Alert, TouchableOpacity, AsyncStorage, StyleSheet } from 'react-native';
+import React, { useState } from "react";
+import {
+  Text,
+  TextInput,
+  View,
+  SafeAreaView,
+  Alert,
+  TouchableOpacity,
+  AsyncStorage,
+  StyleSheet
+} from "react-native";
 
-import api from '../services/api'
+import api from "../services/api";
 
 export default function Book({ navigation }) {
-    const [date, setDate] = useState('');
+  const [date, setDate] = useState("");
 
-    const id = navigation.getParam('id');
+  const id = navigation.getParam("id");
 
-    async function handleSubmit() {
-        const user_id = await AsyncStorage.getItem('user');
+  async function handleSubmit() {
+    const user_id = await AsyncStorage.getItem("user");
 
-        await api.post(`/spots/${id}/bookings`, {
-            date,
-        }, {
-            headers: { user_id }
-        });
+    await api.post(
+      `/spots/${id}/bookings`,
+      {
+        date
+      },
+      {
+        headers: { user_id }
+      }
+    );
 
-        Alert.alert('Solicitação de reserva enviada');
+    Alert.alert("Solicitação de reserva enviada");
 
-        navigation.navigate('List');
-    }
+    navigation.navigate("List");
+  }
 
-    function handleCancel() {
-        navigation.navigate('List');
-    }
+  function handleCancel() {
+    navigation.navigate("List");
+  }
 
-    return (<SafeAreaView style={styles.container}>
-        <Text style={styles.label}>DATA DE INTERESSE *</Text>
-        <TextInput
-            style={styles.input}
-            placeholder='Qual data você quer reservar?'
-            placeholderTextColor='#999'
-            autoCapitalize='words'
-            autoCorrect={false}
-            value={date}
-            onChangeText={setDate}
-        />
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.label}>DATA DE INTERESSE *</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Qual data você quer reservar?"
+        placeholderTextColor="#999"
+        autoCapitalize="words"
+        autoCorrect={false}
+        value={date}
+        onChangeText={setDate}
+      />
 
-        <TouchableOpacity onPress={handleSubmit} style={styles.button}>
-            <Text style={styles.buttonText}>Solicitar reserva</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={handleCancel} style={[styles.button, styles.cancelButton]}>
-            <Text style={styles.buttonText}>Cancelar</Text>
-        </TouchableOpacity>
+      <TouchableOpacity onPress={handleSubmit} style={styles.button}>
+        <Text style={styles.buttonText}>Solicitar reserva</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={handleCancel}
+        style={[styles.button, styles.cancelButton]}
+      >
+        <Text style={styles.buttonText}>Cancelar</Text>
+      </TouchableOpacity>
     </SafeAreaView>
-    )
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        margin: 30,
-        marginTop: 40
-    },
+  container: {
+    margin: 30,
+    marginTop: 40
+  },
 
-    label: {
-        fontWeight: 'bold',
-        color: '#444',
-        marginBottom: 8,
-        marginTop: 40
-    },
+  label: {
+    fontWeight: "bold",
+    color: "#444",
+    marginBottom: 8,
+    marginTop: 40
+  },
 
-    input: {
-        borderWidth: 1,
-        borderColor: '#ddd',
-        paddingHorizontal: 20,
-        fontSize: 16,
-        color: '#444',
-        height: 44,
-        marginBottom: 20,
-        borderRadius: 2
-    },
+  input: {
+    borderWidth: 1,
+    borderColor: "#ddd",
+    paddingHorizontal: 20,
+    fontSize: 16,
+    color: "#444",
+    height: 44,
+    marginBottom: 20,
+    borderRadius: 2
+  },
 
-    button: {
-        height: 42,
-        backgroundColor: '#f05a5b',
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 2,
-        marginTop: 10
-    },
+  button: {
+    height: 42,
+    backgroundColor: "#f05a5b",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 2,
+    marginTop: 10
+  },
 
-    cancelButton: {
-        backgroundColor: '#ccc',
-    },
+  cancelButton: {
+    backgroundColor: "#ccc"
+  },
 
-    buttonText: {
-        color: '#fff',
-        fontWeight: 'bold',
-        fontSize: 16
-    }
-})
+  buttonText: {
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 16
+  }
+});
--- src/pages/List.js
+++ src/pages/List.js
@@ -1,56 +1,69 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import socketio from 'socket.io-client';
+import socketio from "socket.io-client";
 
-import { SafeAreaView, StyleSheet, ScrollView, Image, AsyncStorage, Alert } from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  Image,
+  AsyncStorage,
+  Alert
+} from "react-native";
 
-import list from '../components/SpotList'
-import logo from '../assets/logo.png';
-import SpotList from '../components/SpotList';
+import list from "../components/SpotList";
+import logo from "../assets/logo.png";
+import SpotList from "../components/SpotList";
 
 export default function List() {
-    const [techs, SetTechs] = useState([])
+  const [techs, SetTechs] = useState([]);
 
-    useEffect(() => {
-        AsyncStorage.getItem('user').then(user_id => {
-            const socket = socketio('http://192.168.15.13:3333', {
-                query: { user_id }
-            })
+  useEffect(() => {
+    AsyncStorage.getItem("user").then(user_id => {
+      const socket = socketio("http://192.168.15.13:3333", {
+        query: { user_id }
+      });
 
-            socket.on('booking_response', booking => {
-                Alert.alert(`Sua reserva em ${booking.spot.company} em ${booking.date} foi ${booking.approved ? 'APROVADA' : 'REJEITADA'}`)
-            })
-        })
-    }, [])
+      socket.on("booking_response", booking => {
+        Alert.alert(
+          `Sua reserva em ${booking.spot.company} em ${booking.date} foi ${
+            booking.approved ? "APROVADA" : "REJEITADA"
+          }`
+        );
+      });
+    });
+  }, []);
 
-    useEffect(() => {
-        AsyncStorage.getItem('techs').then(storagedTechs => {
-            const techsArray = storagedTechs.split(',').map(tech => tech.trim());
+  useEffect(() => {
+    AsyncStorage.getItem("techs").then(storagedTechs => {
+      const techsArray = storagedTechs.split(",").map(tech => tech.trim());
 
-            SetTechs(techsArray);
-        })
-    }, [])
+      SetTechs(techsArray);
+    });
+  }, []);
 
-    return (
-        <SafeAreaView style={styles.container}>
-            <Image style={styles.logo} source={logo} />
-            <ScrollView>
-                {techs.map(tech => <SpotList key={tech} tech={tech} />)}
-            </ScrollView>
-        </SafeAreaView>
-    )
+  return (
+    <SafeAreaView style={styles.container}>
+      <Image style={styles.logo} source={logo} />
+      <ScrollView>
+        {techs.map(tech => (
+          <SpotList key={tech} tech={tech} />
+        ))}
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
+  container: {
+    flex: 1
+  },
 
-    logo: {
-        height: 32,
-        resizeMode: 'contain',
-        alignSelf: 'center',
-        marginTop: 40,
-        marginBottom: 30
-    }
-})
+  logo: {
+    height: 32,
+    resizeMode: "contain",
+    alignSelf: "center",
+    marginTop: 40,
+    marginBottom: 30
+  }
+});
